35f0e5606923a4b9b92e3e5506f6505197661f05
mercybwalya <mercybwalya825@gmail.com>
2025-10-06 17:44:41 +0100

    day1.c:hello C program

---
+++ day1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  int main (){
+      char name[100];
+      int age;
+      printf("enter your name:");
+      scanf("%s", &name);
+      printf("enter your age:");
+      scanf("%d",&age);
+    printf("hello %s!you are %d years old\n",name,age);
+   printf("my favorite hobby is coding");
+ return 0;
+}
+
